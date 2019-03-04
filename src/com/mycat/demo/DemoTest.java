dd75307e7f4f75f70f38c9aa25d36f96fca1f858
mikumoge <39541405+mikumoge@users.noreply.github.com>
2019-03-04 14:03:21 +0800

    update from ffx
    update from ffx

--- src/com/mycat/demo/DemoTest.java
+++ src/com/mycat/demo/DemoTest.java
@@ -3,6 +3,7 @@ package com.mycat.demo;
 public class DemoTest {
     public static void main(String[] args) {
         System.out.println("hello World!!!");
+        System.out.println("test push!!!");
         System.out.println("test pull!!!");
     }
 }
