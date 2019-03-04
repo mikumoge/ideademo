320bf6acc3a35e09c0c62765572b9f9911d65f2e
mkmg <1709425856@qq.com>
2019-03-04 12:00:11 +0800

    commit modify DemoTest.java file

--- src/com/mycat/demo/DemoTest.java
+++ src/com/mycat/demo/DemoTest.java
@@ -2,6 +2,7 @@ package com.mycat.demo;
 
 public class DemoTest {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        System.out.println("hello World!!!");
+        System.out.println("test pull!!!");
     }
 }
